75ee5322487bbb0a326537dbdbc540097d9f2f63
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-03-11 14:25:58 +0200

    feat: update DB ( add entry, remove entry)

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from "./UdaciSteppers";
 import DateHeader from "./DateHeader";
 import { Ionicons } from "@expo/vector-icons";
 import TextButton from "./TextButton";
+import { submitEntry, removeEntry } from '../utils/api'
 
 export default class AddEntry extends Component {
     state = {
@@ -26,7 +27,7 @@ export default class AddEntry extends Component {
 
         // Navigate to home
 
-        // Save to "DB"
+        submitEntry({ key, entry })
 
         // Clear local notification
     }
@@ -38,7 +39,7 @@ export default class AddEntry extends Component {
 
         // Route to Home
 
-        // Update "DB"
+        removeEntry(key)
     };
 
 
