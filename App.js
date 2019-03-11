66d138d4c66cc8f31fc7cf169824f6aa05f2fb6c
Loredana Lungu <loredana.lungu@tradeshift.com>
2019-03-11 14:37:44 +0200

    feat: create store

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import AddEntry from "./components/AddEntry";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import reducer from './reducers'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View >
-          <Text>rrrrr</Text>
-        <AddEntry/>
-      </View>
+        <Provider store={createStore(reducer)}>
+          <View >
+            <AddEntry/>
+          </View>
+        </Provider>
     );
   }
 }
